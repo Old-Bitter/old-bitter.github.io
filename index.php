1296adb3dab5fd76e1f0bd263020a8334a76a787
Amber3690 <Amber.renfroe@gmail.com>
2025-07-26 22:35:16 -0700

    switched to php and added includes

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+include 'header.php';
+?>
+
+    <div class="contain">
+        <div class="main">
+            <h4>Week One - July 11th, 2025</h4>
+            <p>I guess I'll just make this a personal log of what I'm learning about. I'm taking <em>Using SQL & SQL Server</em>, <em>Intro to Software Development</em>, and <em>Intro to Programming</em> this summer. I'm hoping to use this to practice and display what I learn from those classes here.</p>
+
+            <h4>Week One - July 9th, 2025</h4>
+            <p>This is my simple website for week one. I haven't made one of these in well over a decade.</p>
+        </div>
+
+ <?php
+include 'footer.php';
+?>
